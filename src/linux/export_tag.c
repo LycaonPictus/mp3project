ca36942f111bd0e6350830938cb79229f4680365
LycaonPictus <lycaonpictuspictus@gmail.com>
2024-08-28 23:26:51 +0200

    0

--- src/linux/export_tag.c
+++ src/linux/export_tag.c
@@ -13,7 +13,7 @@ static void	extract_tag_body(int fd_mp3, int fd_tag, u_int32_t	size)
 	free(body);
 }
 
-void	extract_tag(int fd_mp3, int fd_tag)
+int	extract_tag(char *file_mp3, char *file_tag)
 {
 	char		header[10];
 	u_int32_t	size;
@@ -21,12 +21,32 @@ void	extract_tag(int fd_mp3, int fd_tag)
 	int			fd_mp3;
 	int			fd_tag;
 
+	fd_mp3 = open(file_mp3, O_RDONLY);
+	if (fd_mp3 == -1)
+	{
+		write(2, "Error. File not found.\n", 23);
+		return (1);
+	}
+	if (!access(file_tag, F_OK))
+	{
+		write(2, "Error. File already exists.\n", 28);
+		close(fd_mp3);
+		return (1);
+	}
+	fd_tag = open(file_tag, O_WRONLY | O_APPEND, 666);
+	if (fd_tag == -1)
+	{
+		write(2, "Error. Cannot open file.\n", 25);
+		close(fd_mp3);
+		return (1);
+	}
 	bytes_read = read(fd_mp3, header, 10);
 	write(fd_tag, header, 10);
 	size = get_tag_size(&header[6]);
 	extract_tag_body(fd_mp3, fd_tag, size);
 	close(fd_mp3);
 	close(fd_tag);
+	return (0);
 }
 
 int main()
@@ -34,6 +54,6 @@ int main()
 	char *f1 = "example/Ed Sheeran - Thinking out loud.mp3";
 	char *f2 = "example/Ed Sheeran - Thinking out loud.tag";
 
-	extract_tag(open(f1, O_RDONLY), open(f2, O_CREAT | O_WRONLY));
+	extract_tag(f1, f2);
 	return 0;
 }
